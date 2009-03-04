fa4f5712f57480832070caec44848971e5edce96
danmaq <info@danmaq.com>
2009-03-04 17:39:37 +0000

    ■画面オブジェクト管理クラス 初期画面サイズを取得する機能を追加。

--- trunk/project_flex/doc.bat
+++ trunk/project_flex/doc.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-call "C:\Program Files\Adobe\Flex Builder 3\FlexFrameworkCmdPrompt.bat" "C:\Program Files\Adobe\Flex Builder 3\sdks\3.3.0" "C:\Program Files\Adobe\Flex Builder 3\jre"
+call "C:\Program Files\Adobe\Flex Builder 3\FlexFrameworkCmdPrompt.bat" "C:\Program Files\Adobe\Flex Builder 3\sdks\3.2.0" "C:\Program Files\Adobe\Flex Builder 3\jre"
 asdoc -source-path src -doc-sources src -output asdoc -target-player 10.0.12 -locale ja_JP -main-title "danmaq Nineball Library" -window-title "danmaq Nineball Library"
 PAUSE
