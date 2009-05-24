9a28ef071da55dfefb4e58827de3f03527d4894f
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2009-05-24 12:57:54 +0000

    ■シングルトン機能 新規追加。
    ■内分カウンタ機能
    新規追加。(等速・加速・減速のみ)

---
+++ trunk/XNA/Nineball/Nineball/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("danmaq Nineball-Library")]
+[assembly: AssemblyProduct("danmaq Nineball-Library")]
+[assembly: AssemblyDescription("danmaq Nineball-Library (DNL)")]
+[assembly: AssemblyCompany("danmaq")]
+[assembly: AssemblyCopyright("Copyright © 2009 danmaq All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6faef207-b439-47db-82a3-a73d1ab52772")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersionAttribute("0.0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("ja")]
---
+++ trunk/XNA/Nineball/Nineball/misc/CInterpolate.cs
@@ -0,0 +1,71 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library / (c) 2008-2009 danmaq all rights reserved.
+//		──内分カウンタ機能の関数集
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.Xna.Framework;
+using System;
+
+namespace danmaq.Nineball.misc {
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>内分カウンタ機能の関数集クラス。</summary>
+	public static class CInterpolate {
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>等速変化する内分カウンタです。</summary>
+		/// 
+		/// <param name="fStart"><paramref name="fNow"/>が0と等しい場合の値</param>
+		/// <param name="fEnd"><paramref name="fNow"/>が<paramref name="fLimit"/>と等しい場合の値</param>
+		/// <param name="fNow">現在時間</param>
+		/// <param name="fLimit"><paramref name="fEnd"/>に到達する時間</param>
+		/// <returns>
+		/// 0から<paramref name="fLimit"/>までの<paramref name="fNow"/>に相当する
+		/// <paramref name="fStart"/>から<paramref name="fEnd"/>までの値
+		/// </returns>
+		public static float smooth( float fStart, float fEnd, float fNow, float fLimit ) {
+			if( fNow <= 0.0f ) { return fStart; }
+			if( fNow >= fLimit ) { return fEnd; }
+			return MathHelper.Lerp( fStart, fEnd, fNow / fLimit );
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>減速変化する内分カウンタです。</summary>
+		/// 
+		/// <param name="fStart"><paramref name="fNow"/>が0と等しい場合の値</param>
+		/// <param name="fEnd"><paramref name="fNow"/>が<paramref name="fLimit"/>と等しい場合の値</param>
+		/// <param name="fNow">現在時間</param>
+		/// <param name="fLimit"><paramref name="fEnd"/>に到達する時間</param>
+		/// <returns>
+		/// 0から<paramref name="fLimit"/>までの<paramref name="fNow"/>に相当する
+		/// <paramref name="fStart"/>から<paramref name="fEnd"/>までの値
+		/// </returns>
+		public static float slowdown( float fStart, float fEnd, float fNow, float fLimit ) {
+			if( fNow <= 0.0f ) { return fStart; }
+			if( fNow >= fLimit ) { return fEnd; }
+			return MathHelper.Lerp( fStart, fEnd, 1 - ( float )Math.Pow( 1 - fNow / fLimit, 2 ) );
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>加速変化する内分カウンタです。</summary>
+		/// 
+		/// <param name="fStart"><paramref name="fNow"/>が0と等しい場合の値</param>
+		/// <param name="fEnd"><paramref name="fNow"/>が<paramref name="fLimit"/>と等しい場合の値</param>
+		/// <param name="fNow">現在時間</param>
+		/// <param name="fLimit"><paramref name="fEnd"/>に到達する時間</param>
+		/// <returns>
+		/// 0から<paramref name="fLimit"/>までの<paramref name="fNow"/>に相当する
+		/// <paramref name="fStart"/>から<paramref name="fEnd"/>までの値
+		/// </returns>
+		public static float accelerate( float fStart, float fEnd, float fNow, float fLimit ) {
+			if( fNow <= 0.0f ) { return fStart; }
+			if( fNow >= fLimit ) { return fEnd; }
+			return MathHelper.Lerp( fStart, fEnd, ( float )Math.Pow( fNow / fLimit, 2 ) );
+		}
+
+	}
+}
