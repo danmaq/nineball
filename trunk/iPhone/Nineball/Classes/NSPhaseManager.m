8c532a1fb188ed9355c352d5997815a4652c76e3
danmaq <info@danmaq.com>
2009-07-15 02:05:10 +0000

    ■Nineball(CocoaTouch) ほぼフレ化。

---
+++ trunk/iPhone/Nineball/Classes/NSPhaseManager.m
@@ -0,0 +1,14 @@
+//
+//  NSPhaseManager.m
+//  Nineball
+//
+//  Created by 野村 周平 on 09/07/15.
+//  Copyright 2009 株式会社プロディジ 開発室. All rights reserved.
+//
+
+#import "NSPhaseManager.h"
+
+
+@implementation NSPhaseManager
+
+@end
