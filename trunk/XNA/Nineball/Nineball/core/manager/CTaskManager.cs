a237e8596b8be5e4178e4cb7f4e38bfa08585b6e
danmaq <info@danmaq.com>
2009-07-16 02:58:03 +0000

    ■Nineball(XNA) マイクロスレッド管理クラスを実装。 ロガークラス実装。 タスク管理クラスをガワだけ実装。

---
+++ trunk/XNA/Nineball/Nineball/core/manager/CTaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace danmaq.Nineball.core.manager {
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>
+	/// <para>タスク進行を管理するクラス。</para>
+	/// <para>このクラスにタスクを登録し、そしてこのクラスを通じ実行させます。</para>
+	/// </summary>
+	/// <remarks>複数のタスクを擬似的なマルチタスクで並列実行します。</remarks>
+	public class CTaskManager {
+		// ! TODO : 未実装
+	}
+}
