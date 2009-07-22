14e69344d7dacf03270ea55445328a74edac0226
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2009-07-22 02:00:02 +0000

    ■Nineball(XNA) シーン管理クラス追加。 シーン用基底クラスをガワだけ追加。

---
+++ trunk/XNA/Nineball/Nineball/scene/CSceneBase.cs
@@ -0,0 +1,38 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library / Copyright (c) 2008 danmaq all rights reserved.
+//		──シーン本体の基底クラス
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.Xna.Framework;
+using danmaq.Nineball.core.manager;
+using danmaq.Nineball.core.raw;
+
+namespace danmaq.Nineball.scene {
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>
+	/// <para>シーン本体の基底クラスです。</para>
+	/// <para>
+	/// シーン管理クラスCSceneManagerに登録するタスクを作成するためには、
+	/// このクラスを継承するか、ISceneを実装します。
+	/// </para>
+	/// </summary>
+	public abstract class CSceneBase : IScene {
+
+		public IScene nextScene {
+			get { return null; }
+			set { }
+		}
+
+		public void Dispose() { }
+
+		public bool update( GameTime gameTime ) { return false; }
+
+		public void draw( GameTime gameTime, CSprite sprite ) { }
+
+	}
+}
