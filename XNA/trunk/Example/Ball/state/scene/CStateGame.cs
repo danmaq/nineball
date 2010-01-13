daf68d81196f8087323384a69289e6199b16aced
danmaq <info@danmaq.com>
2010-01-13 06:14:25 +0000

    ■XNA版サンプルゲーム「赤い玉 青い玉 競走ゲーム」 正しくコミットされていなかった

---
+++ XNA/trunk/Example/Ball/state/scene/CStateGame.cs
@@ -0,0 +1,58 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library SAMPLE PROGRAM #1
+//	赤い玉 青い玉 競走ゲーム
+//		Copyright (c) 1994-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using danmaq.nineball.entity;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.ball.state.scene {
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>ゲーム画面シーン。</summary>
+	public sealed class CStateGame : CSceneBase {
+
+		//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* constants ──────────────────────────────-*
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly CStateGame instance = new CStateGame();
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>コンストラクタ。</summary>
+		private CStateGame() : base( "ゲーム画面" ) { }
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の更新処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public override void update( IEntity entity, object privateMembers, GameTime gameTime ) {
+			base.update( entity, privateMembers, gameTime );
+			entity.nextState = CStateTitle.instance;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の描画処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public override void draw( IEntity entity, object privateMembers, GameTime gameTime ) {
+			base.draw( entity, privateMembers, gameTime );
+		}
+	}
+}
