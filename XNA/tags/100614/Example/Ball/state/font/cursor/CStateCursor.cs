2c265dbc6e54c6022048dbbadba832fa1ab124a9
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-06-14 12:26:15 +0000

    GameComponentは追加・削除が遅いようなので、該当クラスを殆ど使わずに済む方法を模索する必要がある。 一旦白紙とするため、バックアップとして残しておく。

---
+++ XNA/tags/100614/Example/Ball/state/font/cursor/CStateCursor.cs
@@ -0,0 +1,93 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library SAMPLE PROGRAM #1
+//	赤い玉 青い玉 競走ゲーム
+//		Copyright (c) 1994-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using danmaq.ball.core;
+using danmaq.ball.entity.font;
+using danmaq.ball.state.scene;
+using danmaq.nineball;
+using danmaq.nineball.entity.input;
+using danmaq.nineball.entity.input.data;
+using danmaq.nineball.state;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.ball.state.font.cursor
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>カーソル制御状態クラス。</summary>
+	public sealed class CStateCursor : CState<CCursor, object>
+	{
+
+		//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* constants ──────────────────────────────-*
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly CStateCursor instance = new CStateCursor();
+
+		/// <summary>入力管理クラス。</summary>
+		private readonly CInput inputManager = CGame.instance.inputManager;
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>ゲーム難易度。</summary>
+		private short m_sLevel = 0;
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>コンストラクタ。</summary>
+		private CStateCursor()
+		{
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の更新処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public override void update(CCursor entity, object privateMembers, GameTime gameTime)
+		{
+			bool bLeftInput = inputManager.dirInputState[(int)EDirection.left].pushLoop;
+			bool bRightInput = inputManager.dirInputState[(int)EDirection.right].pushLoop;
+			if(bLeftInput || bRightInput)
+			{
+				Vector2 locate = entity.locate;
+				short sGap = (short)(bLeftInput ? -1 : 1);
+				m_sLevel += sGap;
+				locate.X += 8 * sGap;
+				if(locate.X < 6)
+				{
+					locate.X = 70;
+					m_sLevel = 8;
+				}
+				else if(locate.X > 70)
+				{
+					locate.X = 6;
+					m_sLevel = 0;
+				}
+				entity.locate = locate;
+			}
+			if(inputManager.buttonStateList[0].push)
+			{
+				CStateGame.instance.level = (ushort)m_sLevel;
+				CStarter.scene.nextState = CStateGame.instance;
+			}
+			base.update(entity, privateMembers, gameTime);
+		}
+	}
+}
---
+++ XNA/tags/100614/Nineball/state/input/legacy/CStatePOV.cs
@@ -0,0 +1,63 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+#if WINDOWS
+
+using danmaq.nineball.entity.input;
+using danmaq.nineball.entity.input.data;
+using Microsoft.DirectX.DirectInput;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.nineball.state.input.legacy
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>レガシ ゲーム コントローラ既定の入力状態。</summary>
+	public sealed class CStatePOV : CStateDefaultBase
+	{
+
+		//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* constants ──────────────────────────────-*
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly CStatePOV instance = new CStatePOV();
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>コンストラクタ。</summary>
+		private CStatePOV()
+			: base(EAxisLegacy.POV)
+		{
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>方向ボタンの状態を更新します。</summary>
+		/// <remarks>この状態では方向ボタンを持たないので、何も処理しません。</remarks>
+		/// 
+		/// <param name="state">最新の入力情報。</param>
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		protected override void refleshAxis(
+			JoystickState state, CInputLegacy entity,
+			CInputLegacy.CPrivateMembers privateMembers, GameTime gameTime)
+		{
+			privateMembers.axisVector = state.getVectorPOV();
+		}
+	}
+}
+
+#endif
