acd8dcf4d3f2912cec1771517181c1298c448ae2
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-06-14 14:15:21 +0000

    ■XNA版 GameComponentを極力使わないようにして作り直してみるテスト。 一旦全削除の上、無害なクラスを復元した。

---
+++ XNA/trunk/Nineball/old/core/data/CResource.cs
@@ -0,0 +1,87 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using danmaq.nineball.util;
+using Microsoft.Xna.Framework.Content;
+
+namespace danmaq.nineball.old.core.data
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>リソースとアセットのペア。</summary>
+	/// <remarks>
+	/// このクラスは旧バージョンとの互換性維持のために残されています。近い将来、順次
+	/// 新バージョンの物と置換されたり、機能自体が削除されたりする可能性があります。
+	/// </remarks>
+	[Obsolete]
+	public sealed class CResource<_T> where _T : class
+	{
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>リソース本体</summary>
+		private _T m_resource;
+
+		/// <summary>アセット名文字列</summary>
+		private string m_strAsset;
+
+		//* ─────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* properties ──────────────────────────────*
+
+		/// <summary>リソース本体</summary>
+		public _T resource
+		{
+			get
+			{
+				return m_resource;
+			}
+			set
+			{
+				m_resource = value;
+			}
+		}
+
+		/// <summary>アセット名文字列</summary>
+		public string asset
+		{
+			get
+			{
+				return m_strAsset;
+			}
+			set
+			{
+				m_strAsset = value;
+			}
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// アセット名に対応したリソースをコンテンツマネージャ経由で読み出します。
+		/// </summary>
+		/// 
+		/// <param name="bForce">リソース本体が<c>null</c>でなくても強制的に再読み込みするかどうか</param>
+		/// <param name="mgrContent">コンテンツマネージャ</param>
+		public bool load(bool bForce, ContentManager mgrContent)
+		{
+			bool bNull = (resource == null);
+			bool bResult = (asset != null && (bForce || bNull));
+			if(bResult)
+			{
+				resource = mgrContent.Load<_T>(asset);
+				CLogger.add("コンテンツ " + asset + " を読込しました。");
+			}
+			return bResult;
+		}
+	}
+}
