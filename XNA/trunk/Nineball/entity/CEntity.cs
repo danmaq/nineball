a3aa4a2c7ff6391a7d58dd4f9020b37da9269ad3
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-06-18 03:25:56 +0000

    ■XNA版 状態クラスと状態を持ったオブジェクト クラスを作成。

---
+++ XNA/trunk/Nineball/entity/CEntity.cs
@@ -0,0 +1,148 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using danmaq.nineball.data;
+using danmaq.nineball.state;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.nineball.entity
+{
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>
+	/// <para>状態を持つオブジェクトの基底クラス。</para>
+	/// <para>
+	/// これを継承するか、または<c>IEntity</c>を実装することで、
+	/// 状態を持つオブジェクトを作ることができます。
+	/// </para>
+	/// <para>また、このクラスに直接状態を持たせて使用することもできます。</para>
+	/// </summary>
+	public class CEntity : IEntity
+	{
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* events ────────────────────────────────*
+
+		/// <summary>状態が遷移された時に呼び出されるイベント。</summary>
+		public event EventHandler<CEventChangedState> changedState;
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>型名のキャッシュ。</summary>
+		private string m_strTypeName = null;
+
+		/// <summary>現在の状態。</summary>
+		private IState m_nowState = CState.empty;
+
+		/// <summary>次の状態。</summary>
+		private IState m_nextState = null;
+
+		/// <summary><c>Update</c>メソッド実行後の動作。</summary>
+		private Action m_actionAfterUpdate;
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>コンストラクタ。</para>
+		/// <para>既定の状態で初期化します。</para>
+		/// </summary>
+		public CEntity()
+		{
+		}
+
+		//* ─────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* properties ──────────────────────────────*
+
+		public IState previousState
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public IState currentState
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>汎用フレーム カウンタを取得します。</summary>
+		/// 
+		/// <value>汎用フレーム カウンタ。</value>
+		public int count
+		{
+			get;
+			protected set;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールドを取得します。
+		/// </summary>
+		/// 
+		/// <value>オブジェクトと状態クラスのみがアクセス可能なフィールド。</value>
+		protected virtual object privateMembers
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>このオブジェクトの状態を含めた文字列情報を取得します。</summary>
+		/// 
+		/// <value>このオブジェクトを示す文字列情報。</value>
+		public override string ToString()
+		{
+			if (m_strTypeName == null)
+			{
+				m_strTypeName = GetType().ToString();
+			}
+			return string.Format("{0} STATE[CUR:{1}, PREV:{2}] {3} MEMBERS:{4}",
+				m_strTypeName, currentState, previousState,
+				Environment.NewLine, privateMembers);
+		}
+
+		public void update(GameTime gameTime)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void draw(GameTime gameTime)
+		{
+			currentState.draw(this, privateMembers, gameTime);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>このオブジェクトの終了処理を行います。</summary>
+		public void Dispose()
+		{
+			changedState = null;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>このオブジェクトに空の状態を設定します。</summary>
+		public virtual void setEmptyState()
+		{
+//			nextState = CState.empty;
+		}
+	}
+}
---
+++ XNA/trunk/Nineball/state/CStateGenerics.cs
@@ -0,0 +1,168 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using danmaq.nineball.entity;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.nineball.state
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>
+	/// <para>状態表現のための空の基底クラス。</para>
+	/// <para>
+	/// これを継承するか、<c>IState</c>を実装することで、状態を表現することが出来ます。
+	/// </para>
+	/// </summary>
+	/// 
+	/// <typeparam name="_E">この状態を設定できる対象オブジェクト。</typeparam>
+	/// <typeparam name="_M">
+	/// オブジェクトと状態クラスのみがアクセス可能なフィールドの型。
+	/// </typeparam>
+	public class CState<_E, _M> : IState<_E, _M> where _E : IEntity
+	{
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>状態が開始された時に呼び出されます。</para>
+		/// <para>このメソッドは、遷移元の<c>teardown</c>よりも後に呼び出されます。</para>
+		/// </summary>
+		/// 
+		/// <param name="entity">この状態を適用されたオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		public virtual void setup(_E entity, _M privateMembers)
+		{
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の更新処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public virtual void update(_E entity, _M privateMembers, GameTime gameTime)
+		{
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の描画処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public virtual void draw(_E entity, _M privateMembers, GameTime gameTime)
+		{
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>オブジェクトが別の状態へ移行する時に呼び出されます。</para>
+		/// <para>このメソッドは、遷移先の<c>setup</c>よりも先に呼び出されます。</para>
+		/// </summary>
+		/// 
+		/// <param name="entity">この状態を終了したオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="nextState">オブジェクトが次に適用する状態。</param>
+		public virtual void teardown(_E entity, _M privateMembers, IState<_E, _M> nextState)
+		{
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>オブジェクトが<c>CState.empty</c>へ移行する時に呼び出されます。</para>
+		/// <para>このメソッドは、遷移先の<c>setup</c>よりも先に呼び出されます。</para>
+		/// </summary>
+		/// <remarks>
+		/// このメソッドが呼び出された時は、通常オブジェクトが終了したことを意味します。
+		/// </remarks>
+		/// 
+		/// <param name="entity">この状態を終了したオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		public virtual void teardown(_E entity, _M privateMembers)
+		{
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>状態が開始された時に呼び出されます。</para>
+		/// <para>このメソッドは、遷移元の<c>teardown</c>よりも後に呼び出されます。</para>
+		/// </summary>
+		/// 
+		/// <param name="entity">この状態を適用されたオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		public void setup(IEntity entity, object privateMembers)
+		{
+			setup((_E)entity, (_M)privateMembers);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の更新処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public void update(IEntity entity, object privateMembers, GameTime gameTime)
+		{
+			update((_E)entity, (_M)privateMembers, gameTime);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の描画処理を実行します。</summary>
+		/// 
+		/// <param name="entity">この状態を適用されているオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public void draw(IEntity entity, object privateMembers, GameTime gameTime)
+		{
+			draw((_E)entity, (_M)privateMembers, gameTime);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>オブジェクトが別の状態へ移行する時に呼び出されます。</para>
+		/// <para>このメソッドは、遷移先の<c>setup</c>よりも先に呼び出されます。</para>
+		/// </summary>
+		/// 
+		/// <param name="entity">この状態を終了したオブジェクト。</param>
+		/// <param name="privateMembers">
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールド。
+		/// </param>
+		/// <param name="nextState">オブジェクトが次に適用する状態。</param>
+		public virtual void teardown(IEntity entity, object privateMembers, IState nextState)
+		{
+			if (nextState == CState.empty)
+			{
+				teardown((_E)entity, (_M)privateMembers);
+			}
+			else
+			{
+				teardown((_E)entity, (_M)privateMembers, (CState<_E, _M>)nextState);
+			}
+		}
+	}
+}
