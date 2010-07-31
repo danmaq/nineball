1b6693953ecd69c393f39bf11f1ddf072a794442
danmaq <info@danmaq.com>
2010-07-31 09:26:27 +0000

    ロールバック。

---
+++ XNA/trunk/Example/Ball/entity/font/CCursor.cs
@@ -0,0 +1,154 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library SAMPLE PROGRAM #1
+//	赤い玉 青い玉 競走ゲーム
+//		Copyright (c) 1994-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using danmaq.ball.state.font.cursor;
+using danmaq.ball.state.font.cursor.view;
+using danmaq.nineball.entity;
+using danmaq.nineball.state;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.ball.entity.font
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>カーソル オブジェクト。</summary>
+	public sealed class CCursor : CEntity
+	{
+
+		//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* constants ──────────────────────────────-*
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly CCursor instance = new CCursor();
+
+		/// <summary>表示用のAI。</summary>
+		private readonly CAI<CCursor> aiView;
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>カーソル位置。</summary>
+		private Vector2 m_locate;
+
+		/// <summary>ワールド空間。</summary>
+		private Matrix m_world = Matrix.Identity;
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>コンストラクタ。</summary>
+		private CCursor()
+			: base(CStateCursor.instance)
+		{
+			locate = Vector2.Zero;
+			aiView = new CAI<CCursor>(this, CStateVisible.instance);
+		}
+
+		//* ─────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* properties ──────────────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>次に変化する状態を設定します。</summary>
+		/// 
+		/// <value>次に変化する状態。</value>
+		/// <exception cref="System.ArgumentNullException">
+		/// 状態として、nullを設定しようとした場合。
+		/// </exception>
+		public new IState<CCursor, Matrix> nextState
+		{
+			set
+			{
+				base.nextState = value;
+			}
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>カーソル位置を設定/取得します。</summary>
+		/// 
+		/// <value>カーソル位置。</value>
+		public Vector2 locate
+		{
+			get
+			{
+				return m_locate;
+			}
+			set
+			{
+				m_locate = value;
+				m_world = Matrix.Identity;
+				m_world.Translation = getCursorTranslation(value);
+			}
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// オブジェクトと状態クラスのみがアクセス可能なフィールドを取得します。
+		/// </summary>
+		/// 
+		/// <value>オブジェクトと状態クラスのみがアクセス可能なフィールド。</value>
+		protected override object privateMembers
+		{
+			get
+			{
+				return m_world;
+			}
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>カーソル座標から3D空間座標へ変換を行います。</summary>
+		/// 
+		/// <param name="locate">カーソル座標。</param>
+		/// <returns>3D空間座標。</returns>
+		public static Vector3 getCursorTranslation(Vector2 locate)
+		{
+			return new Vector3(((int)locate.X - 40f) * 8f, (-(int)locate.Y + 11.5f) * 16f, 0);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>初期化処理を実行します。</summary>
+		public override void initialize()
+		{
+			aiView.initialize();
+			base.initialize();
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>このオブジェクトの終了処理を行います。</summary>
+		public override void Dispose()
+		{
+			aiView.Dispose();
+			base.Dispose();
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の更新処理を実行します。</summary>
+		/// 
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public override void update(GameTime gameTime)
+		{
+			aiView.update(gameTime);
+			base.update(gameTime);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>1フレーム分の描画処理を実行します。</summary>
+		/// 
+		/// <param name="gameTime">前フレームが開始してからの経過時間。</param>
+		public override void draw(GameTime gameTime)
+		{
+			aiView.draw(gameTime);
+			base.draw(gameTime);
+		}
+	}
+}
---
+++ XNA/trunk/Nineball/data/SGradation.cs
@@ -0,0 +1,225 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2010 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using danmaq.nineball.util.math;
+using Microsoft.Xna.Framework;
+
+namespace danmaq.nineball.data
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>グラデーション用情報を格納するクラス。</summary>
+	[Serializable]
+	public struct SGradation
+	{
+
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>初期値</summary>
+		public float start;
+
+		/// <summary>最終値</summary>
+		public float end;
+
+		/// <summary>限界値1</summary>
+		public float limit1;
+
+		/// <summary>限界値2</summary>
+		public float limit2;
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>コンストラクタ。</para>
+		/// <para>
+		/// すべての項目に同一の値を設定します。(グラデーションしません)
+		/// </para>
+		/// </summary>
+		/// 
+		/// <param name="fExpr">値</param>
+		private SGradation(float fExpr)
+		{
+			start = fExpr;
+			end = fExpr;
+			limit1 = fExpr;
+			limit2 = fExpr;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>コンストラクタ。</para>
+		/// <para>
+		/// グラデーション値を設定します。
+		/// </para>
+		/// </summary>
+		/// 
+		/// <param name="fStart">初期値</param>
+		/// <param name="fEnd">最終値</param>
+		/// <param name="fLimit1">限界値1</param>
+		/// <param name="fLimit2">限界値2</param>
+		public SGradation(float fStart, float fEnd, float fLimit1, float fLimit2)
+		{
+			start = fStart;
+			end = fEnd;
+			limit1 = fLimit1;
+			limit2 = fLimit2;
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// グラデーション計算をせず、デフォルトの値を取得します。
+		/// </summary>
+		/// 
+		/// <param name="g">グラデーション用情報 オブジェクト</param>
+		/// <returns>デフォルトの値</returns>
+		public static implicit operator float(SGradation g)
+		{
+			return g.get();
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// すべての項目に同一の値を設定します。(グラデーションしません)
+		/// </summary>
+		/// 
+		/// <param name="f">値</param>
+		/// <returns>グラデーション用情報 オブジェクト</returns>
+		public static implicit operator SGradation(float f)
+		{
+			return new SGradation(f);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーション同士を加算します。</summary>
+		/// 
+		/// <param name="g1">グラデーション用情報 オブジェクト1</param>
+		/// <param name="g2">グラデーション用情報 オブジェクト2</param>
+		/// <returns>グラデーション用情報 オブジェクト</returns>
+		public static SGradation operator +(SGradation g1, SGradation g2)
+		{
+			return new SGradation(
+					g1.start + g2.start, g1.end + g2.end,
+					g1.limit1 + g2.limit1, g1.limit2 + g2.limit2);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーション同士を減算します。</summary>
+		/// 
+		/// <param name="g1">グラデーション用情報 オブジェクト1</param>
+		/// <param name="g2">グラデーション用情報 オブジェクト2</param>
+		/// <returns>グラデーション用情報 オブジェクト</returns>
+		public static SGradation operator -(SGradation g1, SGradation g2)
+		{
+			return new SGradation(
+					g1.start - g2.start, g1.end - g2.end,
+					g1.limit1 - g2.limit1, g1.limit2 - g2.limit2);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーションを乗算します。</summary>
+		/// 
+		/// <param name="g">グラデーション用情報 オブジェクト</param>
+		/// <param name="f">値</param>
+		/// <returns>グラデーション用情報 オブジェクト</returns>
+		public static SGradation operator *(SGradation g, float f)
+		{
+			return new SGradation(g.start * f, g.end * f, g.limit1 * f, g.limit2 * f);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーションを除算します。</summary>
+		/// 
+		/// <param name="g">グラデーション用情報 オブジェクト</param>
+		/// <param name="f">値</param>
+		/// <returns>グラデーション用情報 オブジェクト</returns>
+		public static SGradation operator /(SGradation g, float f)
+		{
+			return new SGradation(g.start / f, g.end / f, g.limit1 / f, g.limit2 / f);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーション値を設定します。</summary>
+		/// 
+		/// <param name="fStart">初期値</param>
+		/// <param name="fEnd">最終値</param>
+		/// <param name="fLimit1">限界値1</param>
+		/// <param name="fLimit2">限界値2</param>
+		public void set(float fStart, float fEnd, float fLimit1, float fLimit2)
+		{
+			start = fStart;
+			end = fEnd;
+			limit1 = fLimit1;
+			limit2 = fLimit2;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// すべての項目に同一の値を設定します。(グラデーションしません)
+		/// </summary>
+		/// 
+		/// <param name="fExpr">値</param>
+		public void set(float fExpr)
+		{
+			set(fExpr, fExpr, fExpr, fExpr);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーション計算をします。</summary>
+		/// 
+		/// <param name="nNow">現在値</param>
+		/// <param name="nSize">分割数</param>
+		/// <returns>補完値</returns>
+		public float smooth(int nNow, int nSize)
+		{
+			if(limit1 == limit2)
+			{
+				return limit1;
+			}
+			return MathHelper.Clamp(CInterpolate._clampSmooth(start, end, nNow, nSize),
+				MathHelper.Min(limit1, limit2), MathHelper.Max(limit1, limit2));
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>グラデーションの合計全長の計算をします。</summary>
+		/// 
+		/// <param name="nSize">分割数</param>
+		/// <returns>全長</returns>
+		public float length(int nSize)
+		{
+			float fResult = 0;
+			for(int i = 0; i < nSize; fResult += smooth(i++, nSize))
+				;
+			return fResult;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// グラデーション計算をせず、デフォルトの値を取得します。
+		/// </summary>
+		/// 
+		/// <returns>デフォルトの値</returns>
+		public float get()
+		{
+			if(limit1 == limit2)
+			{
+				return limit1;
+			}
+			return MathHelper.Clamp(start,
+				MathHelper.Min(limit1, limit2),
+				MathHelper.Max(limit1, limit2));
+		}
+	}
+}
