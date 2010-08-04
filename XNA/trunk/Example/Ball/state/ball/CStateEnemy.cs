33b0de7f47c5867efbec6db64e734cfd9bbef36a
danmaq <info@danmaq.com>
2010-08-04 15:07:47 +0000

    ■XNA版サンプル 一部機能の名前空間変更につきサンプルも対応。

--- XNA/trunk/Example/Ball/state/ball/CStateEnemy.cs
+++ XNA/trunk/Example/Ball/state/ball/CStateEnemy.cs
@@ -11,8 +11,8 @@
 using System;
 using danmaq.ball.entity;
 using danmaq.ball.state.scene;
-using danmaq.nineball.misc;
 using danmaq.nineball.state;
+using danmaq.nineball.util.math;
 using Microsoft.Xna.Framework;
 
 namespace danmaq.ball.state.ball
