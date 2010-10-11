b144e3c35ac99755f35b7ae309058862c2e886c5
danmaq <info@danmaq.com>
2010-10-11 14:58:59 +0000

    ■XNA版 タスク管理クラスを半分ほど実装した。インターフェイスICollection<T>に対応。 旧バージョンで使用していたレイヤ機構は実装取りやめ。 (レイヤの数分だけ、管理クラスぶん回せばいいんじゃね？とふと思ったので)

--- XNA/trunk/Nineball/entity/manager/CTaskManager.cs
+++ XNA/trunk/Nineball/entity/manager/CTaskManager.cs
@@ -7,9 +7,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-using danmaq.nineball.state;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using danmaq.nineball.state;
 
 namespace danmaq.nineball.entity.manager
 {
@@ -23,8 +24,37 @@ namespace danmaq.nineball.entity.manager
 	/// それを回避したい場合はこのクラスを使うとよいでしょう。
 	/// </para>
 	/// </summary>
-	public sealed class CTaskManager : CEntity
+	public sealed class CTaskManager : CEntity, ICollection<ITask>
 	{
+
+		//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+		/// <summary>タスク登録情報。</summary>
+		public struct SRemoveInfo
+		{
+
+			//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+			//* constants ──────────────────────────────-*
+
+			/// <summary>タスク本体。</summary>
+			public readonly ITask task;
+
+			/// <summary>削除時に呼び出される関数。</summary>
+			public readonly Action<ITask> callback;
+
+			//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+			//* constructor & destructor ───────────────────────*
+
+			//* -----------------------------------------------------------------------*
+			/// <summary>コンストラクタ。</summary>
+			/// 
+			/// <param name="task">タスク本体。</param>
+			/// <param name="callback">削除時に呼び出される関数。</param>
+			public SRemoveInfo(ITask task, Action<ITask> callback)
+			{
+				this.task = task;
+				this.callback = callback;
+			}
+		}
 
 		//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
 		/// <summary>オブジェクトと状態クラスのみがアクセス可能なフィールド。</summary>
@@ -41,10 +71,7 @@ namespace danmaq.nineball.entity.manager
 			public readonly List<ITask> add = new List<ITask>();
 
 			/// <summary>削除予約されているタスク一覧。</summary>
-			public readonly List<ITask> remove = new List<ITask>();
-
-			//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
-			//* fields ────────────────────────────────*
+			public readonly List<SRemoveInfo> remove = new List<SRemoveInfo>();
 
 			//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
 			//* methods ───────────────────────────────-*
@@ -65,8 +92,24 @@ namespace danmaq.nineball.entity.manager
 		/// <summary>オブジェクトと状態クラスのみがアクセス可能なフィールド。</summary>
 		private readonly CPrivateMembers _private;
 
+		//* ───-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* fields ────────────────────────────────*
+
+		/// <summary>全削除フラグ。</summary>
+		private bool m_allClear = false;
+
 		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
 		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// <para>コンストラクタ。</para>
+		/// <para>既定の状態で初期化します。</para>
+		/// </summary>
+		public CTaskManager()
+			: this(null)
+		{
+		}
 
 		//* -----------------------------------------------------------------------*
 		/// <summary>
@@ -81,5 +124,118 @@ namespace danmaq.nineball.entity.manager
 			_private = (CPrivateMembers)privateMembers;
 		}
 
+		//* ─────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* properties ──────────────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>登録されているタスクの総数を取得します。</summary>
+		/// 
+		/// <value>登録されているタスクの総数。</value>
+		public int Count
+		{
+			get
+			{
+				return _private.tasks.Count;
+			}
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>この管理クラスが読み取り専用かどうかを取得します。</summary>
+		/// 
+		/// <value>この管理クラスが読み取り専用である場合、<c>true</c>。</value>
+		public bool IsReadOnly
+		{
+			get
+			{
+				return ((ICollection<ITask>)_private.tasks).IsReadOnly;
+			}
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>タスク追加の予約をします。</summary>
+		/// 
+		/// <param name="item">タスク。</param>
+		public void Add(ITask item)
+		{
+			_private.add.Add(item);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>タスク削除の予約をします。</summary>
+		/// 
+		/// <param name="task">タスク。</param>
+		/// <returns><c>true</c>。</returns>
+		public bool Remove(ITask task)
+		{
+			return Remove(task, null);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>タスク削除の予約をします。</summary>
+		/// 
+		/// <param name="task">タスク。</param>
+		/// <param name="callback">削除直前に実行されるアクション。</param>
+		/// <returns><c>true</c>。</returns>
+		public bool Remove(ITask task, Action<ITask> callback)
+		{
+			_private.remove.Add(new SRemoveInfo(task, callback));
+			return true;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>管理しているタスクを全て解放するための予約を入れます。</summary>
+		public void Clear()
+		{
+			m_allClear = true;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>特定の値が格納されているかどうかを判断します。</summary>
+		/// 
+		/// <param name="item">検索するオブジェクト。</param>
+		/// <returns>存在する場合、<c>true</c>。</returns>
+		public bool Contains(ITask item)
+		{
+			throw new NotImplementedException();
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>要素を配列にコピーします。</summary>
+		/// 
+		/// <param name="array">
+		/// 要素がコピーされる1次元かつ0から始まるインデックス番号の配列。
+		/// </param>
+		/// <param name="arrayIndex">
+		/// コピーの開始位置となる、配列の0から始まるインデックス番号。
+		/// </param>
+		public void CopyTo(ITask[] array, int arrayIndex)
+		{
+			_private.tasks.CopyTo(array, arrayIndex);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// 指定した型のコレクションに対する単純な反復処理をサポートする列挙子を公開します。
+		/// </summary>
+		/// 
+		/// <returns>列挙するオブジェクトの型。</returns>
+		public IEnumerator<ITask> GetEnumerator()
+		{
+			return _private.tasks.GetEnumerator();
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>
+		/// 非ジェネリック コレクションに対する反復処理をサポートする列挙子を公開します。
+		/// </summary>
+		/// 
+		/// <returns>列挙するオブジェクトの型。</returns>
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable)_private.tasks).GetEnumerator();
+		}
 	}
 }
