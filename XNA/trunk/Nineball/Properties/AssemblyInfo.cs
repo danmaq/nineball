90f098c3ab7729bce447124bb2ac4e84925c45ec
danmaq <info@danmaq.com>
2010-10-19 13:06:51 +0000

    ■XNA版 スプライト バッチをアニメーションさせるための機能をガワだけ作成。 フレームレート計測機能が全く機能していなかった不具合を修正。 そのついでに計測ロジックを最適化、負荷を微量だが軽くした。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.50")]
+[assembly: AssemblyVersion("0.1.0.51")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
