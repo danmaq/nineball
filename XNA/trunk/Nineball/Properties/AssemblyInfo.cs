e100cb8f60886a08b0d3f9436cce42df41136520
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-11-29 09:15:40 +0000

    ■XNA版 即時状態変化機能をどこからでも使用できるようにした。 最後に状態が変化したフレーム時間を取得する機能を追加。 バージョン表記について、ビルドナンバーをタスクベースからの通しのナンバーで合わせた。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.75")]
+[assembly: AssemblyVersion("0.1.0.76")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
