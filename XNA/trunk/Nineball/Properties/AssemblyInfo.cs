5362f08927d1c76ac3ba497475ee12122d976369
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-12-01 04:31:55 +0000

    ■XNA版 所有者を動的に変更可能な、状態を持つオブジェクト クラスを追加。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.76")]
+[assembly: AssemblyVersion("0.1.0.89")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
