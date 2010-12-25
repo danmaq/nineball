2d8e6712643660c75b8c3ff56a623ea790b38001
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2010-12-25 15:03:15 +0000

    ■XNA版 アニメーション スプライト機能において、Dispose()呼び出し時にデータをクリアするようにした。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.96")]
+[assembly: AssemblyVersion("0.1.0.97")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
