e02fe80395f2fc9cb836443e411bf41bba3e065a
danmaq <info@danmaq.com>
2010-12-29 02:05:25 +0000

    ■XNA版 プロジェクト設定の軽微な変更。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.97")]
+[assembly: AssemblyVersion("0.1.0.98")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
