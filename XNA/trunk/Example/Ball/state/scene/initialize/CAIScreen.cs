08d7ac90d12dd3e76a2e6d49c185c4e7743448d8
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2011-01-10 17:21:29 +0000

    ■XNA版 時間・ヒープ・FPS表示機能を追加。
    ■XNA版サンプル「赤い玉 青い玉 競走ゲーム」
    半年の間にかなり設計が変わり、ビルドできなくなっていたので作り直し。
    とりあえず初期化処理を6割ほど実装。

---
+++ XNA/trunk/Example/Ball/state/scene/initialize/CAIScreen.cs
@@ -0,0 +1,102 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library SAMPLE PROGRAM #1
+//	赤い玉 青い玉 競走ゲーム
+//		Copyright (c) 1994-2011 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using danmaq.ball.core;
+using danmaq.nineball.data;
+using danmaq.nineball.entity.component;
+using danmaq.nineball.entity.graphics;
+using danmaq.nineball.state;
+using danmaq.nineball.util.resolution;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace danmaq.ball.state.scene.initialize
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>画面初期化の状態。</summary>
+	sealed class CAIScreen
+		: CAIBase
+	{
+
+		//* ─────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* constants ──────────────────────────────-*
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly IState instance = new CAIScreen();
+
+		//* ────────────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* constructor & destructor ───────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>コンストラクタ。</summary>
+		private CAIScreen()
+			: base("画面表示の初期化")
+		{
+		}
+
+
+		//* ─────-＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*
+		//* properties ──────────────────────────────*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>次に遷移すべき状態を取得します。</summary>
+		/// 
+		/// <value>次に遷移すべき状態。</value>
+		public override IState nextState
+		{
+			get
+			{
+				return CAIComponents.instance;
+			}
+		}
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>初期化処理を実行します。</summary>
+		public override void initialize()
+		{
+			CGame game = CGame.instance;
+			GraphicsDeviceManager gdm = game.graphicDeviceManager;
+			game.IsMouseVisible = true;
+			game.IsFixedTimeStep = false;
+			gdm.IsFullScreen = false;
+			gdm.SynchronizeWithVerticalRetrace = true;
+			gdm.PreferMultiSampling = true;
+			gdm.ApplyChanges();
+			CSpriteManager sprite = new CSpriteManager();
+			sprite.spriteBatch = new SpriteBatch(gdm.GraphicsDevice);
+			sprite.resolution = new CResolutionLetterBox(EResolution.DCGA, EResolution.VGA, false);
+			CGame.sprite = sprite;
+			initializeViewport();
+			game.GraphicsDevice.RenderState.PointSpriteEnable = true;
+			new CDrawableGameComponent(game, sprite, true);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>ビューポートを初期化します。</summary>
+		private void initializeViewport()
+		{
+			CResolutionLetterBox res = (CResolutionLetterBox)CGame.sprite.resolution;
+			GraphicsDevice device = CGame.instance.GraphicsDevice;
+			Rectangle viewRect = res.convertRectangle(EResolution.VGA.toRect());
+			Viewport viewPort = device.Viewport;
+			viewPort.X = viewRect.X;
+			viewPort.Y = viewRect.Y;
+			viewPort.Width = viewRect.Width;
+			viewPort.Height = viewRect.Height;
+			device.Viewport = viewPort;
+			res.alignHorizontal = EAlign.LeftTop;
+			res.alignVertical = EAlign.LeftTop;
+		}
+	}
+}
