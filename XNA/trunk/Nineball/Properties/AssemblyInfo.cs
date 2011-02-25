a4d08d359708a0422c503326ca64d256d39cf194
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2011-02-25 15:01:28 +0000

    ■XNA版 CSerializeHelper<T>クラスを使って複数ファイルを扱う場合において、データフォルダにゴミフォルダが大量に発生してしまう不具合を修正。 CInputAdapter<T,S>クラスのフォース フィードバック ラッパーを削除。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.110")]
+[assembly: AssemblyVersion("0.2.1.111")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
