5860b39ce49b9b885fce64613f39ddde9394f25b
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2011-08-12 12:39:48 +0000

    ■XNA版 配列操作系の汎用関数を追加。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.119")]
+[assembly: AssemblyVersion("0.2.1.120")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
---
+++ XNA/trunk/Nineball/util/CArray.cs
@@ -0,0 +1,54 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//	danmaq Nineball-Library
+//		Copyright (c) 2008-2011 danmaq all rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace danmaq.nineball.util
+{
+
+	//* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ *
+	/// <summary>配列操作周りの関数集クラス。</summary>
+	public static class CArray
+	{
+
+		//* ────＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿_*
+		//* methods ───────────────────────────────-*
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>配列の値をランダムに入れ替えます。</summary>
+		/// 
+		/// <typeparam name="T">配列の型。</typeparam>
+		/// <param name="array">配列。</param>
+		/// <param name="random">乱数ジェネレータ。</param>
+		public static void sortRandom<T>(T[] array, Random random)
+		{
+			int length = array.Length;
+			if (length >= 2)
+			{
+				for (int i = length; --i >= 0; )
+				{
+					int j = random.Next(i + 1);
+					T tmp = array[j];
+					array[j] = array[i];
+					array[i] = tmp;
+				}
+			}
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>配列の値をインデックス値で埋めます。</summary>
+		/// 
+		/// <param name="array">配列。</param>
+		public static void fillIndex(int[] array)
+		{
+			for (int i = array.Length; --i >= 0; array[i] = i)
+				;
+		}
+	}
+}
