ed8c20a40c53ec427bbc3be0e47bd032bbf398aa
danmaq <danmaq@9d0c5804-f64d-4f04-9779-6b79d024f7f6>
2011-08-16 10:56:20 +0000

    ■XNA版 スレッドプールのアイドル待機機能に、待機した時間を取得する機能を追加。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.120")]
+[assembly: AssemblyVersion("0.2.1.122")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
