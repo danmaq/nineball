d5544259af6a2777561617e26d2f7b8441d75e35
danmaq <info@danmaq.com>
2011-12-19 09:59:30 +0000

    ■XNA版 CEntityクラスに遅延状態変更タイマを常時稼働するようにした。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.148")]
+[assembly: AssemblyVersion("0.2.1.149")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
