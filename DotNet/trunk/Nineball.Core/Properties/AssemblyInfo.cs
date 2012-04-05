a109ef8fbb52a8f3357b2cef826ee7cf84cf0edc
danmaq <info@danmaq.com>
2012-04-05 05:28:51 +0000

    ■.NET版 .NETアプリ共通のCoreエディション作成。

---
+++ DotNet/trunk/Nineball.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// アセンブリに関する全般的な情報は、以下の一連の属性によって管理されます。 
+// アセンブリに関連付けられている情報を変更するには、これらの属性値を変更します。
+// 
+[assembly: AssemblyTitle("danmaq Nineball-Library Core")]
+[assembly: AssemblyProduct("Nineball.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("danmaq")]
+[assembly: AssemblyCopyright("Copyright ©  2012 danmaq All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible 属性を False に設定すると、このアセンブリに含まれる型が 
+// COM コンポーネントに公開されません。COM からこのアセンブリの型にアクセスする必要がある場合は、 
+// その型の ComVisible 属性を True に設定してください。
+// Windows アセンブリだけが COM に対応しています。
+[assembly: ComVisible(true)]
+
+// Windows 上では、このプロジェクトが COM に公開されている場合、 次の GUID が typelib の ID として設定されます。
+// Windows 以外のプラットフォームでは、このアセンブリをデバイスに展開するときに
+// タイトル ストレージ コンテナーを一意に識別するために使用します。
+[assembly: Guid("cea25237-6021-40b0-9c52-6758d7f839f3")]
+
+// アセンブリのバージョン情報は、次の 4 つの値で構成されています。
+//
+//      メジャー バージョン
+//      マイナー バージョン 
+//      ビルド番号
+//      リビジョン番号
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
