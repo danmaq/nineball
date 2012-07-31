6242349dc2f208146dddb18e89ddf24aa44cb2b3
danmaq <info@danmaq.com>
2012-07-31 14:44:24 +0000

    ■XNA版 リストの補助用便利関数を追加。

--- XNA/trunk/Nineball/Properties/AssemblyInfo.cs
+++ XNA/trunk/Nineball/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.162")]
+[assembly: AssemblyVersion("0.2.1.163")]
 [assembly: NeutralResourcesLanguageAttribute( "ja-JP" )]
