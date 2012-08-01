5f53554059f863e4b339bd76bfc33d51888a12ad
danmaq <info@danmaq.com>
2012-08-01 01:40:55 +0000

    ■XNA版 リストの補助用便利関数を少しだけ拡張。

--- XNA/trunk/Nineball/util/collection/CListExtension.cs
+++ XNA/trunk/Nineball/util/collection/CListExtension.cs
@@ -27,7 +27,7 @@ namespace danmaq.nineball.util.collection
 		/// <returns>末尾の要素。</returns>
 		public static _T peekLast<_T>(this IList<_T> list)
 		{
-			return list[list.Count - 1];
+			return peekLast(list, false);
 		}
 
 		//* -----------------------------------------------------------------------*
@@ -37,10 +37,7 @@ namespace danmaq.nineball.util.collection
 		/// <returns>削除された末尾の要素。</returns>
 		public static _T pop<_T>(this IList<_T> list)
 		{
-			int index = list.Count - 1;
-			_T result = list[index];
-			list.RemoveAt(index);
-			return result;
+			return pop(list, false);
 		}
 
 		//* -----------------------------------------------------------------------*
@@ -51,9 +48,71 @@ namespace danmaq.nineball.util.collection
 		/// <returns>置換された要素。</returns>
 		public static _T replaceLast<_T>(this IList<_T> list, _T value)
 		{
+			return replaceLast(list, value, false);
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>リストに要素が存在する場合、末尾の要素を取得します。</summary>
+		/// <remarks>
+		/// <paramref name="validateEmpty"/>が<c>true</c>かつ、要素が存在しない場合、
+		/// 既定値を取得します。
+		/// </remarks>
+		/// 
+		/// <param name="list">対象のリスト。</param>
+		/// <param name="validateEmpty">要素の有無を確認するかどうか。</param>
+		/// <returns>末尾の要素。</returns>
+		public static _T peekLast<_T>(this IList<_T> list, bool validateEmpty)
+		{
 			int index = list.Count - 1;
-			_T result = list[index];
-			list[index] = value;
+			return validateEmpty && index < 0 ? default(_T) : list[index];
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>リストの末尾を削除します。</summary>
+		/// <remarks>
+		/// <paramref name="validateEmpty"/>が<c>true</c>かつ、要素が存在しない場合、
+		/// リストを操作せずに、既定値を取得します。
+		/// </remarks>
+		/// 
+		/// <param name="list">対象のリスト。</param>
+		/// <param name="validateEmpty">要素の有無を確認するかどうか。</param>
+		/// <returns>削除された末尾の要素。</returns>
+		public static _T pop<_T>(this IList<_T> list, bool validateEmpty)
+		{
+			int index = list.Count - 1;
+			_T result = default(_T);
+			if (!(validateEmpty && index < 0))
+			{
+				result = list[index];
+				list.RemoveAt(index);
+			}
+			return result;
+		}
+
+		//* -----------------------------------------------------------------------*
+		/// <summary>リストの末尾を置換します。</summary>
+		/// <remarks>
+		/// <paramref name="validateEmpty"/>が<c>true</c>かつ、要素が存在しない場合、
+		/// リスト末尾に追加し、既定値を取得します。
+		/// </remarks>
+		/// 
+		/// <param name="list">対象のリスト。</param>
+		/// <param name="value">置換する要素。</param>
+		/// <param name="validateEmpty">要素の有無を確認するかどうか。</param>
+		/// <returns>置換された要素。</returns>
+		public static _T replaceLast<_T>(this IList<_T> list, _T value, bool validateEmpty)
+		{
+			int index = list.Count - 1;
+			_T result = default(_T);
+			if (validateEmpty && index < 0)
+			{
+				list.Add(value);
+			}
+			else
+			{
+				result = list[index];
+				list[index] = value;
+			}
 			return result;
 		}
 	}
